573741d2ba341d100cd000c95f30a68571730a24
Jaishree Bala <jaishree1bala@gmail.com>
2022-04-23 21:27:43 -0400

    Add Simon Bell 2024 https://www.linkedin.com/in/simonbell613/

--- js/ece_students.js
+++ js/ece_students.js
@@ -1,56 +1,60 @@
 var ece_students = [
-    {
-      name: "Sayma Khan",
-      website: "https://saymaakhan.github.io/",
-      year: "2024"
-    },
-    {
-      name: "Jaishree Balamurugan",
-      website: "https://jaishreebala.com/",
-      year: "2024"
-    },
-    {
-      name: "Adshayan Balendra",
-      website: "https://adshayanb.github.io/",
-      year: "2024"
-    },
-    {
-      name: "Rishan Ratnasivam",
-      website: "https://www.rishanratna.com/",
-      year: "2024"
-    },
-    {
-      name: "Justin Mendes",
-      website: "https://www.linkedin.com/in/justincmendes/",
-      year: "2024"
-    },
-    {
-      name: "Varun Garg",
-      website: "https://www.linkedin.com/in/varungarg26/",
-      year: "2025"
-    },
-    {
-      name: "Saqib Ali",
-      website: "https://saqibali.ca/",
-      year: "2024"
-    },
-    {
-      name: "Kelly Tan",
-      website: "https://www.linkedin.com/in/k35tan/",
-      year: "2024"
-    },
-    {
-      name: "Azizul Chowdhury",
-      website: "https://www.linkedin.com/in/azizul-c",
-      year: "2026"
-    },
-    {
-      name: "Kaveen Kumarasinghe",
-      website: "https://kaveenk.com",
-      year: "2024"
-    }
+  {
+    name: "Sayma Khan",
+    website: "https://saymaakhan.github.io/",
+    year: "2024",
+  },
+  {
+    name: "Jaishree Balamurugan",
+    website: "https://jaishreebala.com/",
+    year: "2024",
+  },
+  {
+    name: "Adshayan Balendra",
+    website: "https://adshayanb.github.io/",
+    year: "2024",
+  },
+  {
+    name: "Rishan Ratnasivam",
+    website: "https://www.rishanratna.com/",
+    year: "2024",
+  },
+  {
+    name: "Justin Mendes",
+    website: "https://www.linkedin.com/in/justincmendes/",
+    year: "2024",
+  },
+  {
+    name: "Varun Garg",
+    website: "https://www.linkedin.com/in/varungarg26/",
+    year: "2025",
+  },
+  {
+    name: "Saqib Ali",
+    website: "https://saqibali.ca/",
+    year: "2024",
+  },
+  {
+    name: "Kelly Tan",
+    website: "https://www.linkedin.com/in/k35tan/",
+    year: "2024",
+  },
+  {
+    name: "Azizul Chowdhury",
+    website: "https://www.linkedin.com/in/azizul-c",
+    year: "2026",
+  },
 
+  {
+    name: "Kaveen Kumarasinghe",
+    website: "https://kaveenk.com",
+    year: "2024",
+  },
+  {
+    name: "Simon Bell",
+    website: "https://www.linkedin.com/in/simonbell613/",
+    year: "2024",
+  },
+];
 
-  ];
-
-  export {ece_students}
+export { ece_students };
